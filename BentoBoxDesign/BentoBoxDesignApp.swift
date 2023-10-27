f012116a166e2e3c8c9a4a6b744e79df878a9e34
Xinlin Zhou <XZhou@lcs.on.ca>
2023-10-27 11:24:29 -0400

    Initial Commit

---
+++ BentoBoxDesign/BentoBoxDesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  BentoBoxDesignApp.swift
+//  BentoBoxDesign
+//
+//  Created by Fiona ZHOU on 2023-10-27.
+//
+
+import SwiftUI
+
+@main
+struct BentoBoxDesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
